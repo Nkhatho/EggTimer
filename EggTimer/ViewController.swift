6b1d337b59b4bc29ce3f9dd66bcd6e316f71b148
leolenkhatho@gmail.com <leolenkhatho@gmail.com>
2024-05-22 05:28:56 +0200

    added timer and label outlet to display seconds

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let eggTimes: [String : Int] = ["Soft": 300, "Medium": 420, "Hard": 720]
+    var eggTimerCounter: Int = 0
+    var timer: Timer? = nil
+    @IBOutlet weak var labelOutput: UILabel!
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer!.invalidate()
+        eggTimerCounter = eggTimes[sender.currentTitle!]!
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateCounter(counter: Int) {
+        //example functionality
+        if eggTimerCounter == 0 {
+            timer!.invalidate()
+            print("Your egg is ready")
+        }
+        print("\(eggTimerCounter) seconds to egg ready")
+        eggTimerCounter -= 1
+    }
+    
+    
 }
